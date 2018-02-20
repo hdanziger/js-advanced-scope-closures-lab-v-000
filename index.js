010923020553081fd6fbbe16ee52d073504dffb6
EC2 Default User <ec2-user@ip-172-31-22-232.us-east-2.compute.internal>
2018-02-20 21:25:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function produceDrivingRange(blockRange){
+    return function(startBlock, endBlock) {
+        
+    let start = parseInt(startBlock);
+    let end = parseInt(endBlock);
+    let distance = Math.abs(end - start);
+    let difference = blockRange - distance;
+
+        if (difference > 0 ){
+        return `within range by ${difference}`;
+        } else {
+            
+        return `${Math.abs(difference)} blocks out of range`;
+        }
+    }
+}
+
+function produceTipCalculator(tip){
+    return function(totalFare) {
+        let tips = parseFloat(tip)
+        
+        return totalFare*tips
+        
+    }
+}
+
+function createDriver() {
+    let driverId = 0
+    return class {
+        constructor(name) {
+        this.name = name 
+        this.id = ++driverId
+        
+    }
+    }
+}
